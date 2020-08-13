0e6c60a14beaef67e09efdc335bf0aecfc6727f3
Jordan Leich <jordanleich@gmail.com>
2020-08-13 19:01:08 -0400

    v3.0

---
+++ Calculator.py
@@ -0,0 +1,76 @@
+# Program information =================================================================================================|
+# Program created by: Jordan Leich                                                                                     |
+# Program created on: 9/4/19 Last Updated: 8/13/2020                                                                   |
+# Program version 3.0                                                                                                  |
+# =====================================================================================================================|
+
+# Imports
+import colors
+import restart
+
+# Variables
+pi = 3.14
+
+
+def addition(number1, number2):
+    return number1 + number2
+
+
+def subtract(number1, number2):
+    return number1 - number2
+
+
+def multiply(number1, number2):
+    return number1 * number2
+
+
+def power(number1, number2):
+    return number1 ** number2
+
+
+def divide(number1, number2):
+    return number1 / number2
+
+
+# Entire operation
+def Calc():
+    global operator1, result
+    number1 = float(input("What is your first number: "))
+    print()
+    number2 = float(input("What is your second number: "))
+    print()
+    operator1 = str(input('Choose an operator (+, -, *, /, or ** to multiply by a power) '))
+    print()
+
+    if operator1.lower() == "+" or operator1.lower() == "add":
+        print(colors.green, addition(number1, number2), colors.reset, '\n')
+        restart.restart()
+
+    elif operator1.lower() == "-" or operator1.lower() == "subtract":
+        print(colors.green, subtract(number1, number2), colors.reset, '\n')
+        restart.restart()
+
+    elif operator1.lower() == "*" or operator1.lower() == "multiply" or operator1.lower() == "times":
+        print(colors.green, multiply(number1, number2), colors.reset, '\n')
+        restart.restart()
+
+    elif number1 == 0 or number2 == 0 and operator1.lower() == "/" or operator1.lower() == "divide":
+        print(ZeroDivisionError)
+        print(colors.red + "You cannot divide by zero!\n", colors.reset)
+        restart.restart()
+
+    elif operator1.lower() == "/" or operator1.lower() == "divide" or operator1.lower() == "division":
+        print(colors.green, divide(number1, number2), colors.reset, '\n')
+        restart.restart()
+
+    elif operator1.lower() == "**" or operator1.lower() == 'power':
+        print(colors.green, power(number1, number2), colors.reset, '\n')
+        restart.restart()
+
+    else:
+        print(colors.red + 'Operation error found!\n', colors.reset)
+        restart.restart()
+
+
+# Main Engine
+Calc()
